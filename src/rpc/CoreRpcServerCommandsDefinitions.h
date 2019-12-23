c20adb91467a4a4d2c4ce72fc57f61301051f558
SolFly <SolFly86@gmail.com>
2019-12-23 14:56:08 +0200

    Update CoreRpcServerCommandsDefinitions.h

--- src/rpc/CoreRpcServerCommandsDefinitions.h
+++ src/rpc/CoreRpcServerCommandsDefinitions.h
@@ -38,7 +38,15 @@ namespace CryptoNote
             KV_MEMBER(status)
         }
     };
+struct STATUS_STRUCT_EX {
+  std::string status;
+  std::string data;
 
+  void serialize(ISerializer &s) {
+    KV_MEMBER(status)
+    KV_MEMBER(data)
+  }
+};
     struct COMMAND_RPC_GET_HEIGHT
     {
         typedef EMPTY_STRUCT request;
@@ -541,7 +549,7 @@ namespace CryptoNote
     struct COMMAND_RPC_SUBMITBLOCK
     {
         typedef std::vector<std::string> request;
-        typedef STATUS_STRUCT response;
+        typedef STATUS_STRUCT_EX response;
     };
 
     struct block_header_response
